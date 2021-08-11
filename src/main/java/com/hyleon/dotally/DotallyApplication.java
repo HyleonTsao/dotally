3689f5fffd620c2c010ab5508484032068517719
hyleon <hyleontsao@gmail.com>
2021-08-11 13:45:54 +0800

    init commit

---
+++ src/main/java/com/hyleon/dotally/DotallyApplication.java
@@ -0,0 +1,13 @@
+package com.hyleon.dotally;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DotallyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DotallyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hyleon/dotally/DotallyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hyleon.dotally;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DotallyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
